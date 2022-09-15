5de04d05e8fc80faad2b5423296189b4ecb10a95
Ömer YAZICIOĞLU <oyazicioglu@hotmail.com>
2022-09-15 16:05:31 +0300

    Delete ErrorResponse and SuccessResponse

--- src/Response/Base/ErrorResponse.ts
+++
@@ -1,9 +0,0 @@
-import { BaseError } from '../../Error/Base/BaseError';
-import { ErrorValueObject } from '../../Error/Base/ErrorValueObject';
-import { BaseResponse } from './BaseResponse';
-
-export class ErrorResponse extends BaseResponse<ErrorValueObject> {
-    constructor(error: BaseError) {
-        super(undefined, error);
-    }
-}
--- src/Response/Base/SuccessResponse.ts
+++
@@ -1,7 +0,0 @@
-import { BaseResponse } from './BaseResponse';
-
-export class SuccessResponse<T> extends BaseResponse<T> {
-    constructor(data: T) {
-        super(data, undefined);
-    }
-}
