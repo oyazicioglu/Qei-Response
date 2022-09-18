70a4ec67ab0cff2784ba34b249dd64018476a32d
Ömer YAZICIOĞLU <oyazicioglu@hotmail.com>
2022-09-18 23:09:01 +0300

    Fix types

---
+++ index.ts
@@ -0,0 +1,4 @@
+export { BaseError, ErrorType } from './src/BaseError';
+export { BaseErrorObject } from './src/BaseErrorObject';
+export { BaseResponse } from './src/BaseResponse';
+export { IValueObject } from './src/IValueObject';
