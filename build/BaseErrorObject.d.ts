ea160c7ff6fdbe5e2f23114ff202281327275673
Ömer YAZICIOĞLU <oyazicioglu@hotmail.com>
2022-09-18 22:57:42 +0300

    Remove prepare script and add index.d.ts1

--- build/BaseErrorObject.d.ts
+++
@@ -1,6 +0,0 @@
-import { ErrorType } from './BaseError';
-export interface BaseErrorObject {
-    ErrorNumber: number;
-    ErrorType: ErrorType;
-    Message: string;
-}
--- build/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BaseResponse = exports.ErrorType = exports.BaseError = void 0;
-var BaseError_1 = require("./BaseError");
-Object.defineProperty(exports, "BaseError", { enumerable: true, get: function () { return BaseError_1.BaseError; } });
-Object.defineProperty(exports, "ErrorType", { enumerable: true, get: function () { return BaseError_1.ErrorType; } });
-var BaseResponse_1 = require("./BaseResponse");
-Object.defineProperty(exports, "BaseResponse", { enumerable: true, get: function () { return BaseResponse_1.BaseResponse; } });
