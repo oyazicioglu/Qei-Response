387df3ad1c45cd45163d90eda0d9014b7ec32051
Ömer YAZICIOĞLU <oyazicioglu@hotmail.com>
2022-09-18 22:30:31 +0300

    Export types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { BaseError } from './BaseError';
+export { BaseError, ErrorType } from './BaseError';
 export { BaseErrorObject } from './BaseErrorObject';
 export { BaseResponse } from './BaseResponse';
 export { IValueObject } from './IValueObject';
